5ffc796751ccc9c1a882ecb8aeeb957c9f0d68e6
Sergio Martinez-Losa <sergiomtz.losa@gmail.com>
2021-10-18 14:15:39 +0200

    review report bibliography bibunits and chapterbib

--- bibliography-work-report/bibunit-latex-template-report-unizar/latex_bibunits.bat
+++ bibliography-work-report/bibunit-latex-template-report-unizar/latex_bibunits.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 SET FILE=%1
 pdflatex -enable-write18 --shell-escape %FILE%
-FOR /f %%a IN ('dir /b /s "chapters\*.aux"') DO (
+FOR /f %%a IN ('dir /b /s "bu*.aux"') DO (
     bibtex %%a
 )
 pdflatex -enable-write18 --shell-escape %FILE%
