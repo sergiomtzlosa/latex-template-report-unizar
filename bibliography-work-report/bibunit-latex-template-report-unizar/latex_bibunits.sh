27e0702b4192d86b13a13318815dc7d27d9b0d49
Sergio Martinez-Losa <sergiomtz.losa@gmail.com>
2021-10-19 10:07:47 +0200

    review report bibliography bibunits and chapterbib

--- bibliography-work-report/bibunit-latex-template-report-unizar/latex_bibunits.sh
+++ bibliography-work-report/bibunit-latex-template-report-unizar/latex_bibunits.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 FILE=$1
+NAME="${FILE%.*}"
 pdflatex -enable-write18 --shell-escape $FILE
 buFiles=$(ls bu*.aux)
 for f in ${buFiles[@]}; do
     bibtex $f
 done
+bibtex ${NAME}.aux
 pdflatex -enable-write18 --shell-escape $FILE
 pdflatex -enable-write18 --shell-escape $FILE
